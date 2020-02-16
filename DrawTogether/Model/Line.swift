7a9295f9846677d3ec12c19f457461b1e2b7e9de
Lacie <dark830917@gmail.com>
2020-02-16 23:56:46 +0800

    extension Line code able

--- DrawTogether/Model/Line.swift
+++ DrawTogether/Model/Line.swift
@@ -18,3 +18,35 @@ struct Line {
     var points: [CGPoint]
     
 }
+
+extension Line: Codable {
+    
+    init(from decoder: Decoder) throws {
+        var container = try decoder.unkeyedContainer()
+
+        width = try container.decode(Float.self)
+        alpha = try container.decode(Float.self)
+        blur = try container.decode(Float.self)
+        points = try container.decode([CGPoint].self)
+
+        let colorData = try container.decode(Data.self)
+        let shadowData = try container.decode(Data.self)
+        color = try NSKeyedUnarchiver.unarchiveTopLevelObjectWithData(colorData) as? UIColor ?? UIColor.black
+        shadow = try NSKeyedUnarchiver.unarchiveTopLevelObjectWithData(shadowData) as? UIColor ?? UIColor.black
+    }
+    
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.unkeyedContainer()
+        
+        try container.encode(width)
+        try container.encode(alpha)
+        try container.encode(blur)
+        try container.encode(points)
+
+        let colorData = try NSKeyedArchiver.archivedData(withRootObject: color, requiringSecureCoding: false)
+        try container.encode(colorData)
+        
+        let shadowData = try NSKeyedArchiver.archivedData(withRootObject: shadow, requiringSecureCoding: false)
+        try container.encode(shadowData)
+    }
+}
